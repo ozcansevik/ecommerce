3ff53a48359188b53f5bc870daee1b42212134dd
Ozcan SEVIK <sevik.ozcan@gmail.com>
2018-06-02 10:27:55 +0200

    update service to return all Command , all products, stock by product id

---
+++ Ecommerce.SEVIK.DETAXISDUPOET/WCFService/IServiceECommerce.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Modele.MonProjet.Entities;
+
+namespace WCFService
+{
+    [ServiceContract]
+    public interface IServiceECommerce
+    {
+        /*
+         * GET : tous les produits
+         */
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            UriTemplate = "produit_data")]
+        ProduitData GetProduitData();
+
+        /*
+         * GET : toutes les commandes
+         */
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        UriTemplate = "produit_data")]
+        CommandeData GetCommandeData();
+
+        /*
+         * GET : stock d'un produit
+         */
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        RequestFormat = WebMessageFormat.Json,
+        UriTemplate = "stocke?code={code}")]
+        int GetStock(int code);
+
+    }
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    [DataContract]
+    public class ProduitData
+    {
+       
+        [DataMember]
+        public List<Produit> produits
+        {
+            get { return produits; }
+            set { produits = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class CommandeData
+    {
+
+        [DataMember]
+        public List<Commande> commandes
+        {
+            get { return commandes; }
+            set { commandes = value; }
+        }
+
+    }
+}
--- Ecommerce.SEVIK.DETAXISDUPOET/WCFService/Service1.svc.cs
+++ Ecommerce.SEVIK.DETAXISDUPOET/WCFService/Service1.svc.cs
@@ -5,29 +5,41 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using BusinessLayer.MonProjet;
+using Modele.MonProjet.Entities;
 
 namespace WCFService
 {
     // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
     // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
-    public class Service1 : IService1
+    public class ServiceECommerce : IServiceECommerce
     {
-        public string GetData(int value)
+        private BusinessLayerManager blm;
+
+        public ServiceECommerce()
         {
-            return string.Format("You entered: {0}", value);
+            this.blm = BusinessLayerManager.Instance;
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public CommandeData GetCommandeData()
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            CommandeData c = new CommandeData();
+            c.commandes = blm.GetAllCommande();
+            return c;
+        }
+
+        public ProduitData GetProduitData()
+        {
+            ProduitData p = new ProduitData();
+            p.produits = blm.GetAllProduit();
+            return p;
+
+        }
+
+        public int GetStock(int code)
+        {
+            Produit p = blm.GetProduit(code);
+            return p.Stock;
         }
     }
 }
